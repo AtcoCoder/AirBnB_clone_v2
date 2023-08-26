b9cc591c5e630de1fefa72d5b3898f8276a6fd70
Omar Jammeh <orms45@gmail.com>
2023-08-26 13:18:58 +0000

    updated user class, added primary key

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ from sqlalchemy import Column, String, Integer
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+    id = Column(String(60), primary_key=True)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
