539cc6f7abd0b4462ad39a4bbe1b8b801902f324
Omar Jammeh <orms45@gmail.com>
2023-10-05 18:08:49 +0000

    added the correct path for the tar file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,10 +3,13 @@
  the function do_pack"""
 from fabric.api import *
 from datetime import datetime
+import os
 
 
 def do_pack():
     """Generates archive of all file in web_static folder"""
-
+    path = "versions/"
+    if not os.path.exists(path):
+        os.mkdir(path)
     created_at = datetime.now().strftime('%Y%m%d%H%M%S')
-    local('tar -czvf web_static_{}.tgz web_static'.format(created_at))
+    local('tar -czvf {}web_static_{}.tgz web_static'.format(path, created_at))
