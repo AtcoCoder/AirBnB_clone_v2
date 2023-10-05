39ffbb884b59d24c3b6548ddae79f0ea928e2c10
Omar Jammeh <orms45@gmail.com>
2023-10-05 17:48:19 +0000

    Compress files before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Script that distributes an archive to web servers using
- the function do_play"""
+ the function do_pack"""
 from fabric.api import *
 from datetime import datetime
 
 
-def do_deploy():
+def do_pack():
     """Generates archive of all file in web_static folder"""
 
     created_at = datetime.now().strftime('%Y%m%d%H%M%S')
-    local('tar -czvf web_static_{}'.format(created_at))
+    local('tar -czvf web_static_{} web_static'.format(created_at))
