1709944a93ba64517bbbfa4bd2865f56395eeab9
Omar Jammeh <orms45@gmail.com>
2023-10-06 00:05:20 +0000

    hot fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,22 +11,15 @@ def do_deploy(archive_path):
     """Generates archive of all file in web_static folder"""
     if not os.path.exists(archive_path):
         return False
-    put(archive_path, '/tmp/')
-    archive_file = run('ls /tmp/ | grep web_static_')
-    if archive_file.failed:
-        return False
-    extracted_files = archive_file.split('.')[0]
-    release_path = '/data/web_static/releases/{}'.format(extracted_files)
-    result = sudo('tar -xvf {} /tmp/{}'.format(release_path, archive_file))
-    if result.failed:
-        return False
-    result = sudo('rm /tmp/{}'.format(archive_file))
-    if result.failed:
-        return False
-    result = sudo('unlink /data/web_static/current')
-    if result.failed:
-        return False
-    result = sudo('ln -s {} /data/web_static/current'.format(release_path))
-    if result.failed:
+    try:
+        put(archive_path, '/tmp/')
+        archive_file = run('ls /tmp/ | grep web_static_')
+        extracted_files = archive_file.split('.')[0]
+        release_path = '/data/web_static/releases/{}'.format(extracted_files)
+        sudo('tar -xvf {} /tmp/{}'.format(release_path, archive_file))
+        sudo('rm /tmp/{}'.format(archive_file))
+        sudo('unlink /data/web_static/current')
+        sudo('ln -s {} /data/web_static/current'.format(release_path))
+    except Exception:
         return False
     return True
