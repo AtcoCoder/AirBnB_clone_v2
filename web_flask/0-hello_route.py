3964d945fff4a9fdd0287599ae0ce67f88f1119e
Omar Jammeh <orms45@gmail.com>
2023-10-22 23:45:49 +0000

    Hello Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,14 @@
+#!/usr/bin/python3
+"""My web application"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
+    """home route function"""
     return "Hello HBNB!"
 
 
