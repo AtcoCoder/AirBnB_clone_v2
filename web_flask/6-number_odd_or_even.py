3a046478e2ed554b91bfdd69c7a6d9ac7871ecc1
Omar Jammeh <orms45@gmail.com>
2023-10-23 00:56:36 +0000

    Odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -50,5 +50,15 @@ def number_template(n):
     return render_template('5-number.html', number=n)
 
 
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """Check if number is odd or even and display on browser"""
+    state = 'odd'
+    if n % 2 == 0:
+        state = 'even'
+    text = f"Number: {n} is {state}"
+    return render_template('6-number_odd_or_even.html', text=text)
+
+
 if __name__ == "__main__":
     app.run()
