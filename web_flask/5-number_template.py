b997eda4f53fd91c7a5875fd52cd07288dad3897
Omar Jammeh <orms45@gmail.com>
2023-10-23 00:41:31 +0000

    Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""My web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """home route function"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """hbnb route function"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """Displays our text"""
+    new_text = text.replace('_', ' ')
+    return f"C {new_text}"
+
+
+@app.route('/python/', strict_slashes=False)
+def python_():
+    """python is cool"""
+    return "Python is cool"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    new_text = text.replace('_', ' ')
+    return f"Python {new_text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run()
