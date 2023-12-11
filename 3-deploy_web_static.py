3ce646a5795d13e092ef9600b8dcf1f60ed417fe
Omar Jammeh <orms45@gmail.com>
2023-12-11 07:23:08 +0000

    full deployment update

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,6 +7,8 @@ from fabric.api import put
 from fabric.api import run
 
 env.hosts = ['18.207.1.231', '52.91.153.215']
+env.username = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_pack():
