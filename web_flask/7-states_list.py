a173e2be47a50e5577c3f7da8e309d6665ab3e04
Omar Jammeh <orms45@gmail.com>
2024-01-01 13:14:03 +0000

    Task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def get_states():
+    all_states = storage.all(State)
+    states = [state for state_key, state in all_states.items()]
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """Closes current session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
