d7e088e3e29eadbe14c23aac6dd44693c84bad2d
Omar Jammeh <orms45@gmail.com>
2024-01-01 13:18:47 +0000

    Task 8 update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Starts flask web app that listen on 0.0.0.0:5000"""
 from models import storage
 from flask import Flask, render_template
 from models.state import State
@@ -8,6 +9,7 @@ app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def get_states():
+    """Renders webpage to show all states"""
     all_states = storage.all(State)
     states = [state for state_key, state in all_states.items()]
     return render_template('7-states_list.html', states=states)
